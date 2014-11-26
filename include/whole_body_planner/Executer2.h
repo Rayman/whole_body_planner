dd4d91dea7204e9d7d42f1a3535ced590c218223
Ramon Wijnands <rayman747@hotmail.com>
2014-11-26 15:28:57 +0100

    start Executor2 for the new wbc actionlib

---
+++ include/whole_body_planner/Executer2.h
@@ -0,0 +1,10 @@
+#ifndef EXECUTER2_H
+#define EXECUTER2_H
+
+class Executer2
+{
+public:
+    Executer2();
+};
+
+#endif // EXECUTER2_H
---
+++ src/Executer2.cpp
@@ -0,0 +1,5 @@
+#include "whole_body_planner/Executer2.h"
+
+Executer2::Executer2()
+{
+}
